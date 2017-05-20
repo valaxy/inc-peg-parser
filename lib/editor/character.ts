b2e351ee7a633a6309053beba0dae3c7d4e79118
valaxy <valaxy@qq.com>
2017-05-20 18:01:19 +0800

    update

---
+++ lib/editor/character.ts
@@ -0,0 +1,30 @@
+import ParsingNode from '../parsingNode'
+
+export default class Character {
+    private _next: Character
+    private _parsingNode: ParsingNode
+
+    get next() { return this._next }
+
+    get parsingNode() { return this._parsingNode }
+
+    appendNext(next: Character) {
+        this._next = next
+    }
+
+    insertAfter(ch: Character) {
+        let next = this.next
+        ch.appendNext(next)
+        this._next = ch
+    }
+
+    static createByText(text: string) {
+        let lastCharacter = null
+        text.split('').reverse().map(c => {
+            let ch = new Character
+            ch.appendNext(lastCharacter)
+            lastCharacter = ch
+        })
+        return lastCharacter
+    }
+}
---
+++ lib/editor/editor.ts
@@ -0,0 +1,21 @@
+import Character from './character'
+import Parser from '../session'
+
+export default class Editor {
+    private _parser: Parser
+
+
+    append(text: string, ch: Character?) {
+        let { head, tail } = Character.createByText(text))
+        ch.insertAfter(ch)
+        this._parser.insertAfter(ch.parsingNode, head.ParsingNode, tail.ParsingNode)
+    }
+
+    prepend(text: string, ch: Character?) {
+
+    }
+
+    remove(ch: Character, count: number) {
+
+    }
+}
